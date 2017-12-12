1ad3a0bea25f3fffce340440d1332498a8898d9c
Mike Green <nekbet@gmail.com>
2017-12-12 12:41:40 -0700

    static folder

--- dataServer/settings.py
+++ dataServer/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'HOST': os.environ['db_hostname'],
         'PORT': os.environ['db_port'],
         'OPTIONS': {
-                    'ssl': {'ca': '/static/rds-combined-ca-bundle.pem' }
+                    'ssl': {'ca': '/app/static/rds-combined-ca-bundle.pem' }
         }
     }
 }   
