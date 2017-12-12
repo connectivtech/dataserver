f53a7b0474d85369b262d7f4021fd0d6b36797e0
Mike Green <nekbet@gmail.com>
2017-12-11 21:40:02 -0700

    jfc

--- dataServer/settings.py
+++ dataServer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
 from secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
